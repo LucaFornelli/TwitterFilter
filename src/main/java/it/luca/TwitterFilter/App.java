c4fe53ecabec40bbb13298c34e19b2597f631138
Luca Fornelli <lucamagnolie@gmail.com>
2019-03-25 09:19:26 +0100

    Refactoring Twitter4JStreamer class, and managed filter input property
    Removed sensitive data

--- src/main/java/it/luca/TwitterFilter/App.java
+++ src/main/java/it/luca/TwitterFilter/App.java
@@ -1,82 +1,29 @@
 package it.luca.TwitterFilter;
 
-import it.luca.TwitterFilter.core.Twitter4JInstanceCreator;
+import it.luca.TwitterFilter.core.Twitter4JStreamer;
 import it.luca.TwitterFilter.util.PropertyUtil;
-import twitter4j.FilterQuery;
-import twitter4j.StallWarning;
-import twitter4j.Status;
-import twitter4j.StatusDeletionNotice;
-import twitter4j.StatusListener;
-import twitter4j.TwitterStream;
 
 /**
  *
  */
-public class App 
-{
-	private static Long startTime = System.currentTimeMillis();
-	private static Long elapsedTime;
-	private static Long totalPosts = 0L;
-	private static long pps = Integer.valueOf(PropertyUtil.getPropertyValue("PostsPerSecond"));
-	
-    public static void main( String[] args )
-    {
-	    StatusListener listener = new StatusListener() {
-	 
-	    	@Override
-            public void onStatus(Status status) {
-                if (checkPostPerSecondAverage()) {
-                	System.out.println("@" + status.getUser().getScreenName() + " - " + status.getText());
-                }
-            }
+public class App {
+	public static void main(String[] args) {
 
-            @Override
-            public void onDeletionNotice(StatusDeletionNotice statusDeletionNotice) {
-                System.out.println("Got a status deletion notice id:" + statusDeletionNotice.getStatusId());
-            }
+		String filter = PropertyUtil.getPropertyValue("filters");
+		String[] filters;
 
-            @Override
-            public void onTrackLimitationNotice(int numberOfLimitedStatuses) {
-            	
-            }
+		if (filter == null) {
+			System.out.println("The property \"filters\" must be present into the config.properties file");
+		} else if (filter.equals("")) {
+			Twitter4JStreamer.sampleSearch();
+		} else {
+			if (filter.contains(",")) {
+				filters = filter.split(",");
+			} else {
+				filters = new String[] { filter };
+			}
 
-            @Override
-            public void onScrubGeo(long userId, long upToStatusId) {
-                System.out.println("Got scrub_geo event userId:" + userId + " upToStatusId:" + upToStatusId);
-            }
-
-            @Override
-            public void onStallWarning(StallWarning warning) {
-                System.out.println("Got stall warning:" + warning);
-                
-            }
-
-            @Override
-            public void onException(Exception ex) {
-                ex.printStackTrace();
-            }
-	    };
-	    
-	    
-	    FilterQuery query = new FilterQuery();
-	    query.track(new String[]{"java"});
-	    
-	    TwitterStream twitterStream = Twitter4JInstanceCreator.getTwitterStream();
-
-	    twitterStream.addListener(listener);
-	    twitterStream.filter(query);
-    	
-    }
-    
-    public static boolean checkPostPerSecondAverage() {
-    	elapsedTime = System.currentTimeMillis()-startTime;
-    	double dElapsedTime = Double.valueOf(elapsedTime);
-    	double dTotalPost = Double.valueOf(totalPosts);
-    	double average = ((dTotalPost+1) / (dElapsedTime/1000));
-    	if (pps > average) {
-        	totalPosts++;
-        	return true;
-    	} else
-    		return false;
-    }
+			Twitter4JStreamer.filteredSearch(filters);
+		}
+	}
 }
--- src/main/java/it/luca/TwitterFilter/core/Twitter4JStreamer.java
+++ src/main/java/it/luca/TwitterFilter/core/Twitter4JStreamer.java
@@ -1,48 +1,122 @@
 package it.luca.TwitterFilter.core;
 
-import twitter4j.Query;
+import it.luca.TwitterFilter.util.PropertyUtil;
+import twitter4j.FilterQuery;
 import twitter4j.StallWarning;
 import twitter4j.Status;
 import twitter4j.StatusDeletionNotice;
 import twitter4j.StatusListener;
 import twitter4j.TwitterStream;
-import twitter4j.TwitterStreamFactory;
 
 public class Twitter4JStreamer {
-	public static void streamFeed() {
-	 
+	
+	private static Long startTime = System.currentTimeMillis();
+	private static Long elapsedTime;
+	private static Long totalPosts = 0L;
+	private static long pps = Integer.valueOf(PropertyUtil.getPropertyValue("PostsPerSecond"));
+	
+	public static void filteredSearch(String[] filter) {
+		
 	    StatusListener listener = new StatusListener() {
 	 
-	        @Override
-	        public void onException(Exception e) {
-	            e.printStackTrace();
-	        }
-	        @Override
-	        public void onDeletionNotice(StatusDeletionNotice arg) {
-                System.out.println("Got a status deletion notice id:" + arg.getStatusId());
-	        }
-	        @Override
-	        public void onScrubGeo(long userId, long upToStatusId) {
-                System.out.println("Got scrub_geo event userId:" + userId + " upToStatusId:" + upToStatusId);
-	        }
-	        @Override
-	        public void onStallWarning(StallWarning warning) {
-                System.out.println("Got stall warning:" + warning);
-	        }
-	        @Override
-	        public void onStatus(Status status) {
-                System.out.println("@" + status.getUser().getScreenName() + " - " + status.getText());
-	        }
-	        @Override
-	        public void onTrackLimitationNotice(int numberOfLimitedStatuses) {
-                System.out.println("Got track limitation notice:" + numberOfLimitedStatuses);
-	        }
+	    	@Override
+            public void onStatus(Status status) {
+                if (checkPostPerSecondAverage()) {
+                	System.out.println("@" + status.getUser().getScreenName() + " - " + status.getText());
+                }
+            }
+
+            @Override
+            public void onDeletionNotice(StatusDeletionNotice statusDeletionNotice) {
+//                System.out.println("Got a status deletion notice id:" + statusDeletionNotice.getStatusId());
+            }
+
+            @Override
+            public void onTrackLimitationNotice(int numberOfLimitedStatuses) {
+            	
+            }
+
+            @Override
+            public void onScrubGeo(long userId, long upToStatusId) {
+//                System.out.println("Got scrub_geo event userId:" + userId + " upToStatusId:" + upToStatusId);
+            }
+
+            @Override
+            public void onStallWarning(StallWarning warning) {
+//                System.out.println("Got stall warning:" + warning);
+                
+            }
+
+            @Override
+            public void onException(Exception ex) {
+                ex.printStackTrace();
+            }
 	    };
-	    TwitterStream twitterStream = new TwitterStreamFactory().getInstance();
+	    
+	    TwitterStream twitterStream = Twitter4JInstanceCreator.getTwitterStream();
 	 
 	    twitterStream.addListener(listener);
+	    FilterQuery query = new FilterQuery();
+	    query.track(filter);
+	    
+	    twitterStream.filter(query);
+
+	}
+	
+	public static void sampleSearch() {
+		
+	    StatusListener listener = new StatusListener() {
 	 
+	    	@Override
+            public void onStatus(Status status) {
+                if (checkPostPerSecondAverage()) {
+                	System.out.println("@" + status.getUser().getScreenName() + " - " + status.getText());
+                }
+            }
+
+            @Override
+            public void onDeletionNotice(StatusDeletionNotice statusDeletionNotice) {
+//                System.out.println("Got a status deletion notice id:" + statusDeletionNotice.getStatusId());
+            }
+
+            @Override
+            public void onTrackLimitationNotice(int numberOfLimitedStatuses) {
+            	
+            }
+
+            @Override
+            public void onScrubGeo(long userId, long upToStatusId) {
+//                System.out.println("Got scrub_geo event userId:" + userId + " upToStatusId:" + upToStatusId);
+            }
+
+            @Override
+            public void onStallWarning(StallWarning warning) {
+//                System.out.println("Got stall warning:" + warning);
+                
+            }
+
+            @Override
+            public void onException(Exception ex) {
+                ex.printStackTrace();
+            }
+	    };
+	    
+	    TwitterStream twitterStream = Twitter4JInstanceCreator.getTwitterStream();
+	 
+	    twitterStream.addListener(listener);
 	    twitterStream.sample();
 	}
+	
+	public static boolean checkPostPerSecondAverage() {
+    	elapsedTime = System.currentTimeMillis()-startTime;
+    	double dElapsedTime = Double.valueOf(elapsedTime);
+    	double dTotalPost = Double.valueOf(totalPosts);
+    	double average = ((dTotalPost+1) / (dElapsedTime/1000));
+    	if (pps > average) {
+        	totalPosts++;
+        	return true;
+    	} else
+    		return false;
+    }
 }
 
